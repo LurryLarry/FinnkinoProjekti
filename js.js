2ae7873a8f0e696164e3e8f25253b15e4d250b8d
LurryLarry <43970352+LurryLarry@users.noreply.github.com>
2019-12-03 23:55:58 +0200

    valmis

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function haeTiedot() {
 
-  var teatteri = document.getElementById("teatteri").value;
+  var teatteri = document.getElementById("teatteri").value;  // haetaan form controllin valinnan arvo ja lisätään se urliin
   console.log(teatteri);
   var url = "https://www.finnkino.fi/xml/Schedule/?area=" + teatteri + "&dt=";
   var xmlhttp = new XMLHttpRequest();
@@ -11,24 +11,24 @@ function haeTiedot() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
 
-      var xmlDoc = xmlhttp.responseXML;
+      var xmlDoc = xmlhttp.responseXML;  // laitetaan urlin xml tiedot muuttujaan
 
       console.log(xmlDoc);
 
-      pic = xmlDoc.getElementsByTagName("EventLargeImageLandscape");
+      pic = xmlDoc.getElementsByTagName("EventLargeImageLandscape");  // asetetaan xmldocin osia muuttujiin
       title = xmlDoc.getElementsByTagName("Title");
-     
-      showstart = xmlDoc.getElementsByTagName("dttmShowStart");
-      showend = xmlDoc.getElementsByTagName("dttmShowEnd");
       rating = xmlDoc.getElementsByTagName("RatingImageUrl");
       osto = xmlDoc.getElementsByTagName("ShowURL");
       tiedot = xmlDoc.getElementsByTagName("EventURL");
       console.log(tiedot.innerHTML);
 
       var out = '<div class="card-columns">';
-      for (i = 0; i <= title.length; i++) {
-        
-        var title = xmlDoc.getElementsByTagName("Title")[i].firstChild.nodeValue;
+      for (i = 0; i <= title.length; i++) {  // loopataan otsikoiden mukaan
+        var showstart = xmlDoc.getElementsByTagName("dttmShowStart")[i].innerHTML.slice(11, 16);  // formatoidaan aika objektit
+        var showend = xmlDoc.getElementsByTagName("dttmShowEnd")[i].innerHTML.slice(11, 16);
+        var title = xmlDoc.getElementsByTagName("Title")[i].firstChild.nodeValue; 
+        // luodaan muuttujia, koodin selkeyden vuoksi minusta hyvä tapa sijoitella elementteihin verrattuna esim. pic[i].childNodes[0].nodeValue,
+        // ei tosin jostain syystä toiminut kaikkiin kohtiin.
         var sali = xmlDoc.getElementsByTagName("TheatreAndAuditorium")[i].firstChild.nodeValue;
         out += '<div class="col-12">';
         out += '<div class="card blue-grey lighten-4">';
@@ -38,7 +38,8 @@ function haeTiedot() {
         out += '<h3>' + title + '</h3>';
         out += '</span>';
         out += "<p>" + sali + "</p>";
-        out += '<p><img src="' + rating[i].childNodes[0].nodeValue + '" align="right"></p>'
+        out += '<p>' + showstart + " - " + showend +'</p>';
+        out += '<p><img src="' + rating[i].childNodes[0].nodeValue + '" align="right" alt="Ikärating"></p>'
         out += '<a id="osto" class="btn btn-success" href="' + osto[i].innerHTML + '">Varaa</a>';
         out += '<a id="tiedot" class="btn btn-info" href="' + tiedot[i].innerHTML + '">Esittely</a>';
         out += '</div>';
@@ -49,7 +50,7 @@ function haeTiedot() {
     
       };
       out += '</div>';
-      document.getElementById("kontentti").innerHTML = out;
+      document.getElementById("kontentti").innerHTML = out;  // lisätään luuppaukset kontentti paraan
     
    
      
