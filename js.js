f23597fd5c408069f68d810bc1ca05e0d6a6501a
LurryLarry <lari.vesterinen@gmail.com>
2019-12-03 16:52:50 +0200

    minor fix

--- js.js
+++ js.js
@@ -45,20 +45,20 @@ function haeTiedot() {
       for (i = 0; i <= title.length; i++) {
         
         var title = xmlDoc.getElementsByTagName("Title")[i].firstChild.nodeValue;
-        out += '<div class="col-4 s6 m6">';
+        out += '<div class="col-12">';
         out += '<div class="card blue-grey lighten-4">';
         out += '<img class="card-img-top" src="' + pic[i].childNodes[0].nodeValue + '" alt="Elokuvan kuva" style=width:100%">';
         out += '<div class="card-content">';
         out += '<span class="card-title">';
         out += '<h3>' + title + '</h3>';
-        out += "<p>" + title + "</p>";
         out += '</span>';
+        out += "<p>" + title + "</p>";
 
         out += '</div>';
         out += '</div>';
         out += '</div>';
 
-        
+       
     
       };
       out += '</div>';
